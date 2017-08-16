7680745221c650c5692fc3a471e830441e3d6342
abohannon <abo46n2@gmail.com>
2017-08-16 12:28:26 -0700

    Refactored a lot of code, simplified everything.

--- src/index.js
+++ src/index.js
@@ -1,24 +1,75 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import logo from './logo.svg';
-import './index.css';
 import registerServiceWorker from './registerServiceWorker';
-import RawInput from './components/rawinput';
-import MarkdownOutput from './components/output.js';
 
-// create component
+// create components
 
-class App extends Component {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: '',
+      outputValue: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({inputValue: event.target.value});
+  }
+
   render() {
     return (
-      <div className="inner">
+      <div className="inner" style={{textAlign: 'center'}}>
         <h2>React Markdown Previewer</h2>
-        <RawInput />
-        <MarkdownOutput />
+        <UserInput input={this.state.inputValue} handleInput={this.handleChange}/>
+        <UserOutput input={this.state.inputValue}/>
       </div>
     );
   }
 }
 
-ReactDOM.render(<App />, document.querySelector('.container'));
+class UserInput extends React.Component {
+  render() {
+
+    const textareaStyle = {
+      width: "100%",
+      height: "75vh",
+      border: "1px solid grey"
+    };
+
+    return (
+      <div className="col-md-6 input-area">
+        <h1>Input</h1>
+        <textarea className="user-input" style={textareaStyle} value={this.props.input} onChange={this.props.handleInput}/>
+      </div>
+    );
+  }
+}
+
+class UserOutput extends React.Component {
+  render() {
+
+    const outputStyle = {
+      width: "100%",
+      height: "75vh",
+      textAlign: 'left'
+    };
+
+    return (
+      <div className="col-md-6 output-area">
+        <h1>Output</h1>
+        <div style={outputStyle}>
+          <p>{this.props.input}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+// render app
+
+ReactDOM.render(
+  <App/>, document.querySelector('.container'));
 registerServiceWorker();
