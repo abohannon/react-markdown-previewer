541e3ea9e122b458ca6c71fa69d5155064624e28
abohannon <abo46n2@gmail.com>
2017-08-16 14:22:46 -0700

    Successfully implemented markdown library and rendered proper markup

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 
+import Marked from 'marked';
+
 // create components
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      inputValue: '',
+      inputValue: 'Heading\n=======\n\nSub-heading\n-----------\n \n### Another deeper heading\n \nParagraphs are separated\nby a blank line.\n\nLeave 2 spaces at the end of a line to do a  \nline break\n\nText attributes *italic*, **bold**, \n`monospace`, ~~strikethrough~~ .\n\nShopping list:\n\n  * apples\n  * oranges\n  * pears\n\nNumbered list:\n\n  1. apples\n  2. oranges\n  3. pears\n\nThe rain---not the reign---in\nSpain.\n\n',
       outputValue: ''
     };
 
@@ -50,22 +52,27 @@ class UserInput extends React.Component {
 }
 
 class UserOutput extends React.Component {
+
+
+  renderMarkup(){
+    return {
+      __html: Marked(this.props.input, {sanitize: true})
+    };
+  }
+
   render() {
 
     const outputStyle = {
       width: "100%",
       height: "75vh",
       textAlign: "left",
-      whiteSpace: "pre-wrap",
       wordWrap: "break-word"
     };
 
     return (
       <div className="col-md-6 output-area">
         <h1>Output</h1>
-        <div style={outputStyle}>
-          <p>{this.props.input}</p>
-        </div>
+        <div style={outputStyle} dangerouslySetInnerHTML={this.renderMarkup()} />
       </div>
     );
   }
